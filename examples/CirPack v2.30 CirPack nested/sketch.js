4f6c7716645dc604db6dca9b2eda7179c90c04b6
jesmehta <jesmehta@gmail.com>
2025-08-29 13:20:05 +0530

    Nested circles example

---
+++ examples/CirPack v2.30 CirPack nested/sketch.js
@@ -0,0 +1,99 @@
+// let rL, rH, n, k, rMax;
+let myFoam;
+let r0Min,r0Max,count,attempts,rMax;    //global because mouseClicked(), draw() is also using these
+
+function setup()
+{
+  createCanvas(800,500);
+
+  //for density control
+  //balance small radii range with large numbers and vice versa
+  r0Min = 25;
+  r0Max = 25;
+  rMax = 200;
+
+  count = 7000;
+  attempts = 5000;
+
+  myFoam = getCirPack(r0Min,r0Max,count,attempts,rMax);
+
+  // print(myFoam.length);
+  colorMode(HSB);
+
+}
+
+function draw()
+{
+  background(250);
+
+  for(let i = 0;i<myFoam.length;i++)
+  {
+    noFill();
+    // stroke(0);
+    // myFoam[i].display();
+
+    let xd = myFoam[i].x;
+    let yd = myFoam[i].y;
+    let rd = myFoam[i].bubRadius;
+    
+    // let h = random(360);
+    let h = map(rd,r0Min*2,rMax/2,0,360);
+    fill(h,50,100);
+    stroke(h,70,100);
+
+
+    for(let i = rd;i>2;i-=10)
+    {
+      ellipse(xd,yd,i,i);
+    }
+    // fill('green');
+    // if(myFoam[i].bubRadius>=rMax)
+    // {
+    //   fill('red');
+    // }
+    // ellipse(xd,yd,rd,rd);
+
+  }
+
+  // dispParameters();
+  noLoop();
+
+}
+
+// helper function to display paramters when testing and comparing
+// adhoc position and size values
+function dispParameters()        
+{
+  
+  noStroke();
+  fill(255,220);
+  // rect(width*0.4-10,height-30, width*0.55+10,30,10);
+  rect(width*0.5-10,height-60, width*0.5,50,10);
+  
+  textSize(18);
+  fill(0);
+  let str = "rInitMin " + r0Min + " rInitMax " + r0Max + " rMax " + rMax + " count " + myFoam.length + " max count " + count + " attempts " + attempts;
+  print(str);
+  text(str, width*0.5, height-50, width*0.5, 50);
+}
+
+function mouseClicked()
+{
+  background(250);
+  myFoam.splice(0,myFoam.length);
+  print("spliced", myFoam.length);
+  myFoam = getCirPack(r0Min,r0Max,count,attempts,rMax);
+  draw();
+}
+
+function keyTyped()
+{ 
+  // print("Keypressed");
+  if(key === 's')
+  {
+  saveCanvas("CirPack Tests " + frameCount +".jpg");
+  print("file saved");
+
+  }
+  return false;
+}
