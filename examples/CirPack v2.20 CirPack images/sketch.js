ac7992489ec8db734b3e37a48772e42c26b8b605
jesmehta <jesmehta@gmail.com>
2025-08-29 13:18:58 +0530

    Orange image example

---
+++ examples/CirPack v2.20 CirPack images/sketch.js
@@ -0,0 +1,110 @@
+// let rL, rH, n, k, rMax;
+let myFoam;
+let r0Min,r0Max,count,attempts,rMax;    //global because mouseClicked(), draw() is also using these
+let sourceImg;
+
+function preload()
+{
+  sourceImg = loadImage("naranja2.webp");
+  // sourceImg = loadImage("noun-orange.png");
+  print("sourceImg loaded");
+
+}
+
+
+function setup()
+{
+  createCanvas(800,500);
+// image(img, width/2, height/2);
+  //for density control
+  //balance small radii range with large numbers and vice versa
+  r0Min = 25;
+  r0Max = 35;
+  rMax = 200;
+
+  count = 700;
+  attempts = 5000;
+  imageMode(CENTER);
+  myFoam = getCirPack(r0Min,r0Max,count,attempts,rMax);
+
+  // print(myFoam.length);
+
+}
+
+function draw()
+{
+  background(250);
+
+  for(let i = 0;i<myFoam.length;i++)
+  {
+    noFill();
+    stroke('orange');
+    myFoam[i].display();
+
+    let xd = myFoam[i].x;
+    let yd = myFoam[i].y;
+    let rd = myFoam[i].bubRadius;
+
+    // print(xd, yd, rd);
+
+    let new_img = sourceImg.get();
+    new_img.resize(rd,0);
+    image(new_img,xd,yd);
+
+    // let rd = myFoam[i].bubRadius*0.7;
+    // fill('green');
+    // if(myFoam[i].bubRadius>=rMax)
+    // {
+    //   fill('red');
+    // }
+    // ellipse(xd,yd,rd,rd);
+
+  }
+
+  // noLoop();
+
+  // dispParameters();
+
+}
+
+// helper function to display paramters when testing and comparing
+// adhoc position and size values
+function dispParameters()        
+{
+  
+  noStroke();
+  fill(255,220);
+  // rect(width*0.4-10,height-30, width*0.55+10,30,10);
+  rect(width*0.5-10,height-60, width*0.5,50,10);
+  
+  textSize(18);
+  fill(0);
+  let str = "rInitMin " + r0Min + " rInitMax " + r0Max + " rMax " + rMax + " count " + myFoam.length + " max count " + count + " attempts " + attempts;
+  print(str);
+  text(str, width*0.5, height-50, width*0.5, 50);
+}
+
+function mouseClicked()
+{
+  background(250);
+  myFoam.splice(0,myFoam.length);
+  print("spliced", myFoam.length);
+  myFoam = getCirPack(r0Min,r0Max,count,attempts,rMax);
+}
+
+function keyTyped()
+{ 
+  // print("Keypressed");
+  if(key === 's')
+  {
+  saveCanvas("CirPack Tests " + frameCount +".jpg");
+  print("file saved");
+
+  }
+  return false;
+}
+
+// Make sure p5 sees your handlers even if something changes script context
+window.preload = preload;
+window.setup   = setup;
+window.draw    = draw;
